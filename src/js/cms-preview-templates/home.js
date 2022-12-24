efb04bd36239d3fc0e7a56f04b90d90a727b910c
Miran Mirza <miranmirza@gmail.com>
2022-12-24 09:19:06 +0300

    Fixed preview for hero

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -9,19 +9,14 @@ export default class PostPreview extends React.Component {
     let image = getAsset(entry.getIn(["data", "image"]));
 
     // Bit of a nasty hack to make relative paths work as expected as a background image here
-    if (image && !image.fileObj) {
-        image = window.parent.location.protocol + "//" + window.parent.location.host + image;
+    if (image) {
+        let {path} = image;
+        path = path.split('static/').pop();
+        image = `${window.parent.location.protocol}//${window.parent.location.host}/${path}`;
     }
 
     return <div>
         <Jumbotron image={image} title={entry.getIn(["data", "title"])} subtitle={entry.getIn(["data", "subtitle"])}/>
-
-        <div className="bg-grey-1 pv4">
-          <div className="flex-l mhn1-l ph3 center mw7">
-            <h2 className="f2 b lh-title mb2 w-40-l">{entry.getIn(["data", "blurb", "heading"])}</h2>
-            <p className="w-60-l mb0">{entry.getIn(["data", "blurb", "text"])}</p>
-          </div>
-        </div>
 
         <div className="bg-off-white pv4">
           <div className="ph3 mw7 center">
